2efc602acad7dbe7db2d57ffe8378224daca79bc
Pikking <786575907@qq.com>
2021-05-29 09:21:52 +0800

    {}

---
+++ log/1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+date=`date +%y%m%d%s`
+echo $date
+file="${date}.log"
+commitTip="Add File ${file}"
+echo $file
+touch $file
+echo $commitTip
+git add .
+git commit -a -m"{$commentTip}"
+git push
+
