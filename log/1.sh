61d0e003b2bd38adb6de694887cfb26f3b2653d2
Pikking <786575907@qq.com>
2021-05-29 09:43:36 +0800

    Add File 2105291622252616.log

--- log/1.sh
+++ log/1.sh
@@ -3,9 +3,10 @@ date=`date +%y%m%d%s`
 echo $date
 file="${date}.log"
 commitTip="Add File ${file}"
-echo $file
 touch $file
 echo $commitTip
+chmod 0777 $file
+echo $date > "./${file}"
 git add .
 git commit -a -m"$commitTip"
 git push
