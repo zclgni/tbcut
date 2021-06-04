201adda6678c3020bf992b333810c94d322c22ed
Pikking <786575907@qq.com>
2021-06-04 11:07:01 +0800

    Add File 2106041622776021.log

---
+++ log/a.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+date=`date +%y%m%d%s`
+echo $date
+file="${date}.log"
+commitTip="Add File ${file}"
+touch $file
+echo $commitTip
+chmod 0777 $file
+echo $date > "./${file}"
+git add .
+git commit -a -m"$commitTip"
+git push
+
+file_number=$(ls | grep log | wc -l)
+echo $file_number
+if [ $file_number > 100 ];then
+#        rm -rf ./*.log
+fi
+
+#rm -rf "./${file}"
